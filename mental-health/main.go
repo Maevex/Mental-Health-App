4da7e8487b8b53ed1f3a76d617b8981cf28ab916
Maevex <ilhamku2005@gmail.com>
2025-04-24 01:17:41 +0700

    t

--- mental-health/main.go
+++ mental-health/main.go
@@ -7,7 +7,6 @@ import (
 	"mental-health/routes"
 	"net/http"
 
-	"github.com/gorilla/handlers"
 	"github.com/joho/godotenv"
 )
 
@@ -22,12 +21,6 @@ func main() {
 
 	r := routes.SetupRoutes()
 
-	cors := handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://localhost:8081"}),                   // Origin frontend kamu
-		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "PUT", "DELETE"}), // Metode yang diizinkan
-		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),           // Headers yang diizinkan
-	)
-
 	fmt.Println("Server running on port http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", cors(r)))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
